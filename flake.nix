ae662921c0a3d452cd24562005db4994a0d991d5
Franciszek Strzeżek <fr.strzezek@gmail.com>
2024-08-14 16:58:00 +0200

    .

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     nixpkgs-edge.url = "github:nixos/nixpkgs/nixos-unstable";
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
